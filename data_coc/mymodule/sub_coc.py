5fd104f759747f2e68690dbc8561e02d86bee3e4
coob <koosanghwi@gmail.com>
2023-11-25 10:50:15 +0900

    ver 1.16

--- data_coc/mymodule/sub_coc.py
+++ data_coc/mymodule/sub_coc.py
@@ -342,14 +342,14 @@ def sub_click(cla):
                 full_path = "c:\\my_games\\coc\\data_coc\\imgs\\tuto\\click\\sub_ready.PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(30, 130, 150, 185, cla, img, 0.7)
+                imgs_ = imgs_set_(30, 130, 150, 185, cla, img, 0.65)
                 if imgs_ is not None and imgs_ != False:
                     click_pos_reg(imgs_.x, imgs_.y, cla)
                 else:
                     full_path = "c:\\my_games\\coc\\data_coc\\imgs\\tuto\\click\\sub_ready_click2.PNG"
                     img_array = np.fromfile(full_path, np.uint8)
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(30, 130, 150, 185, cla, img, 0.7)
+                    imgs_ = imgs_set_(30, 130, 150, 185, cla, img, 0.65)
                     if imgs_ is not None and imgs_ != False:
                         click_pos_reg(imgs_.x, imgs_.y, cla)
                     else:
@@ -375,7 +375,7 @@ def sub_click(cla):
                                 full_path = "c:\\my_games\\coc\\data_coc\\imgs\\tuto\\click\\tuto_move_1.PNG"
                                 img_array = np.fromfile(full_path, np.uint8)
                                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                                imgs_ = imgs_set_(140, 130, 200, 185, cla, img, 0.8)
+                                imgs_ = imgs_set_(140, 130, 200, 185, cla, img, 0.7)
                                 if imgs_ is not None and imgs_ != False:
                                     click_pos_reg(imgs_.x, imgs_.y, cla)
 
