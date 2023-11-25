8fef83e5169227ba5c27c4e17c4c916ae1d1ee45
coob <koosanghwi@gmail.com>
2023-11-25 10:27:03 +0900

    ver 1.15

--- data_coc/mymodule/stop_event18.py
+++ data_coc/mymodule/stop_event18.py
@@ -12,7 +12,9 @@ sys.path.append('C:/my_games/' + str(v_.game_folder) + '/' + str(v_.data_folder)
 def _stop_please(cla):
     import numpy as np
     import cv2
+    import os
     from function_game import imgs_set_, click_pos_reg, click_pos_2
+    from massenger import line_to_me
 
     try:
         print("_stop_please")
@@ -24,13 +26,16 @@ def _stop_please(cla):
             print("18_1")
             click_pos_reg(imgs_.x, imgs_.y, cla)
         #     time.sleep(1)
-        # full_path = "c:\\my_games\\" + str(v_.game_folder) + "\\" + str(v_.data_folder) + "\\imgs\\18\\18_2.PNG"
-        # img_array = np.fromfile(full_path, np.uint8)
-        # img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        # imgs_ = imgs_set_(70, 670, 300, 770, cla, img, 0.7)
-        # if imgs_ is not None and imgs_ != False:
-        #     print("18_1")
-        #     click_pos_reg(imgs_.x, imgs_.y, cla)
+        full_path = "c:\\my_games\\" + str(v_.game_folder) + "\\" + str(v_.data_folder) + "\\imgs\\check\\jangsigan.PNG"
+        img_array = np.fromfile(full_path, np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        imgs_ = imgs_set_(370, 480, 470, 530, cla, img, 0.7)
+        if imgs_ is not None and imgs_ != False:
+            print("jangsigan")
+            why = "장시가니다..."
+            line_to_me(cla, why)
+            time.sleep(60000)
+            os.execl(sys.executable, sys.executable, *sys.argv)
 
 
 
