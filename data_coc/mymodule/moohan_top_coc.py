dc9ea47abb9342084e37d0711e07949cb10248f1
coob <koosanghwi@gmail.com>
2023-11-28 07:55:11 +0900

    ver 1.31

--- data_coc/mymodule/moohan_top_coc.py
+++ data_coc/mymodule/moohan_top_coc.py
@@ -213,7 +213,7 @@ def moohan_top_in(cla, where):
                     full_path = "c:\\my_games\\coc\\data_coc\\imgs\\dungeon\\moohan_top_in.PNG"
                     img_array = np.fromfile(full_path, np.uint8)
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(600, 900, 960, 1060, cla, img, 0.8)
+                    imgs_ = imgs_set_(740, 400, 820, 440, cla, img, 0.7)
                     if imgs_ is not None and imgs_ != False:
 
                         for i in range(10):
