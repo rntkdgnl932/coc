06a4613b5b2321e5958dcca40d2e0783051d22c3
coob <koosanghwi@gmail.com>
2023-11-28 08:19:32 +0900

    ver 1.32

--- data_coc/mymodule/dungeon_coc.py
+++ data_coc/mymodule/dungeon_coc.py
@@ -47,7 +47,8 @@ def dungeon_start(cla, where):
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
                 imgs_ = imgs_set_(400, 130, 700, 700, cla, img, 0.7)
                 if imgs_ is not None and imgs_ != False:
-                    click_pos_reg(imgs_.x, imgs_.y, cla)
+                    print("죽었다...")
+                    click_pos_reg(imgs_.x - 100, imgs_.y + 300, cla)
                     for i in range(10):
                         full_path = "c:\\my_games\\coc\\data_coc\\imgs\\dungeon\\underus_migoong_map_title1.PNG"
                         img_array = np.fromfile(full_path, np.uint8)
