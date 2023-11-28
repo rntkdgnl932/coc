9f94ba68d50af0383b047339a662849cac1b989b
coob <koosanghwi@gmail.com>
2023-11-28 11:05:17 +0900

    ver 1.36

--- data_coc/mymodule/stop_event18.py
+++ data_coc/mymodule/stop_event18.py
@@ -36,6 +36,18 @@ def _stop_please(cla):
             line_to_me(cla, why)
             time.sleep(60000)
             os.execl(sys.executable, sys.executable, *sys.argv)
+        else:
+            full_path = "c:\\my_games\\" + str(v_.game_folder) + "\\" + str(
+                v_.data_folder) + "\\imgs\\check\\loss_connection.PNG"
+            img_array = np.fromfile(full_path, np.uint8)
+            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+            imgs_ = imgs_set_(450, 490, 550, 520, cla, img, 0.7)
+            if imgs_ is not None and imgs_ != False:
+                print("loss_connection")
+                why = "서버 연결 끊어짐...서버점검..."
+                line_to_me(cla, why)
+                time.sleep(60000)
+                os.execl(sys.executable, sys.executable, *sys.argv)
 
 
 
