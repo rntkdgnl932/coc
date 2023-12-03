6ca9fa96d078563eafcdf45f3c4f5619a70ddcaf
coob <koosanghwi@gmail.com>
2023-12-04 04:07:51 +0900

    ver 1.72

--- data_coc/mymodule/dungeon_coc.py
+++ data_coc/mymodule/dungeon_coc.py
@@ -22,6 +22,13 @@ def dungeon_start(cla, where):
     try:
         print("dungeon_start", where)
 
+        full_path = "c:\\my_games\\coc\\data_coc\\imgs\\potion\\confrim_move.PNG"
+        img_array = np.fromfile(full_path, np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        imgs_ = imgs_set_(470, 580, 630, 630, cla, img, 0.7)
+        if imgs_ is not None and imgs_ != False:
+            click_pos_reg(imgs_.x - 200, imgs_.y, cla)
+
         result_dun_where = where.split("_")
 
         # result_dun_where[0] => 던전
