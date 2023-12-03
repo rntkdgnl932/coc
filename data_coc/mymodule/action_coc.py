c37cfde56a39ebb643cee5ae64177773fdf7f468
coob <koosanghwi@gmail.com>
2023-12-04 03:54:07 +0900

    ver 1.71

--- data_coc/mymodule/action_coc.py
+++ data_coc/mymodule/action_coc.py
@@ -39,10 +39,15 @@ def confirm_all(cla):
                 imgs_ = imgs_set_(470, 580, 630, 630, cla, img, 0.7)
                 if imgs_ is not None and imgs_ != False:
                     click_pos_reg(imgs_.x, imgs_.y, cla)
-                    jadong_go = True
                     break
                 time.sleep(0.2)
-
+        else:
+            full_path = "c:\\my_games\\coc\\data_coc\\imgs\\potion\\confrim_move.PNG"
+            img_array = np.fromfile(full_path, np.uint8)
+            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+            imgs_ = imgs_set_(470, 580, 630, 630, cla, img, 0.7)
+            if imgs_ is not None and imgs_ != False:
+                click_pos_reg(imgs_.x, imgs_.y, cla)
 
 
     except Exception as e:
